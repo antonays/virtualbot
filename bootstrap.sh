bab3bd604d0260b77d73e3673086ed80ee7f8a43
antona <antonays@gmail.ccc>
2016-11-15 12:12:31 +0100

    added sourcing

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
-echo "starting ..."
-echo "i am $(whoami)"
-echo "================="
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+
+echo "${yellow}starting ..."
+echo "${yellow}i am $(whoami)"
+echo "${yellow}================="
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
 
@@ -16,51 +21,50 @@ sudo apt-get install git -y
 sudo apt-get install build-essential -y
 sudo apt-get install python-catkin-pkg -y
 sudo apt-get install nginx -y
-
-echo "finished basic framework installations ..."
+echo "${yellow}finished basic framework installations ..."
 
 sudo apt-get install python-pip -y
 sudo apt-get install python-virtualenv -y
+sudo apt-get install avahi-deamon -y
 sudo pip install virtualenvwrapper
-echo "finished pip, virtualenv, virtualenvwrapper installations ..."
 sudo pip install uwsgi
+echo "${yellow}finishd secondary framework installations ..."
 
 source /usr/local/bin/virtualenvwrapper.sh
 mkvirtualenv dotbot
-echo "created workspace ..."
+echo "${yellow}created workspace - entering to test..."
 workon dotbot
 deactivate
-echo "exited from workspace ..."
+echo "${yellow}exited from workspace ..."
 
 if [ ! -d "~/project" ]; then
-  echo "creating project directory ..."
+  echo "${yellow}creating project directory ..."
   mkdir ~/project
   git clone https://github.com/dotbot-io/dotbot_ros
-  echo "created project directory ..."
-  echo "cloned ros directory to project ..."
+  echo "${yellow}cloned ros directory to project ..."
 fi
 
 if [ ! -d "~/ros" ]; then
-  echo "creating ros directory ..."
+  echo "${yellow}creating ros directory ..."
   mkdir -p ~/ros/dotbot_ws/src
   bash
   source /opt/ros/kinetic/setup.bash
   cd ~/ros/dotbot_ws/src
   catkin_init_workspace
-  echo "initialized catkin workspace ..."
+  echo "${yellow}initialized catkin workspace ..."
   git clone https://github.com/dotbot-io/dotbot_ros
   cd ..
   catkin_make
-  echo "finished catkin_make ..."
+  echo "${yellow}finished catkin_make ..."
   sudo su -c "source /opt/ros/kinetic/setup.bash; catkin_make install -DCMAKE_INSTALL_PREFIX=/opt/ros/kinetic"
-  echo "installed ros directory ..."
+  echo "${yellow}installed ros directory in root..."
 fi
 
 workon dotbot
 mkdir ~/.virtualenvs/dotbot/project
 cd ~/.virtualenvs/dotbot/project/
 git clone https://github.com/dotbot-io/robot-manager.git
-echo "cloned project manager application ..."
+echo "${yellow}cloned project manager application ..."
 cd robot-manager/
 
 pip install -r requirements.txt
@@ -68,23 +72,31 @@ echo "installed project dependencies ..."
 
 
 sudo rm -rf ~/ros/dotbot_ws/src/dotbot_ros
-echo "removed source code from ros/src ..."
+echo "${yellow}removed source code from ros/src ..."
 
-echo "setting up nginx configurations ..."
+echo "${yellow}setting up nginx configurations ..."
 mkdir ~/.virtualenvs/dotbot/run
 sudo mkdir /var/log/uwsgi
 sudo touch /var/log/uwsgi/uwsgi-dotbot.log
 sudo rm /etc/nginx/sites-available/default 
 sudo wget -O /etc/nginx/sites-available/robot-manager https://raw.githubusercontent.com/dotbot-io/server_config/master/robot-manager
-echo "copied nginx file ..."
+echo "${yellow}copied nginx file ..."
 sudo wget -O /etc/supervisor/conf.d/uwsgi-dotbot.conf https://raw.githubusercontent.com/dotbot-io/server_config/master/uwsgi-dotbot.conf
-echo "copied supervisor config file ..."
+echo "${yellow}copied supervisor config file ..."
+sudo wget -O /etc/avahi/avahi-daemon.conf https://raw.githubusercontent.com/dotbot-io/server_config/master/avahi-daemon.conf
+echo "${yellow}copied avahi config file ..."
 sudo ln -s /etc/nginx/sites-available/robot-manager /etc/nginx/sites-enabled
 sudo service supervisor restart
 sudo service nginx restart 
 deactivate
 
+echo "${yellow}adding sources to bash start..."
+cd ~
+echo "export LC_ALL='en_US.UTF-8'" >> .profile
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> .bashrc
+echo "source /opt/ros/kinetic/setup.bash" >> .bashrc
+echo "source /home/vagrant/ros/dotbot_ws/devel/setup.bash" >> .bashrc
 
-echo "Done installation"
+echo "${yellow}Done installation"
 
 sudo apt-get clean
